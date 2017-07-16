aa2606cc4bd88f01f655b8c5e1fbbb01920c7ec9
Jürgen Dufner <juergen@jdufner.de>
2017-07-16 22:38:15 +0200

    Added WebController with two templates.

---
+++ src/main/java/de/jdufner/watch/tracker/controller/AbweichungController.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2017.
+ *
+ * This file is part of Watchtracker.
+ *
+ * Watchtracker is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Watchtracker is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Watchtracker.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Diese Datei ist Teil von Watchtracker.
+ *
+ * Watchtracker ist Freie Software: Sie können es unter den Bedingungen
+ * der GNU General Public License, wie von der Free Software Foundation,
+ * Version 3 der Lizenz oder (nach Ihrer Wahl) jeder späteren
+ * veröffentlichten Version, weiterverbreiten und/oder modifizieren.
+ *
+ * Watchtracker wird in der Hoffnung, dass es nützlich sein wird, aber
+ * OHNE JEDE GEWÄHRLEISTUNG, bereitgestellt; sogar ohne die implizite
+ * Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
+ * Siehe die GNU General Public License für weitere Details.
+ *
+ * Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
+ * Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
+ */
+
+package de.jdufner.watch.tracker.controller;
+
+import de.jdufner.watch.tracker.businessobjects.Abweichung;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+/**
+ * @author Jürgen Dufner
+ * @since 0.0
+ */
+@Controller
+public class AbweichungController {
+
+  private static final Logger log = LoggerFactory.getLogger(AbweichungController.class);
+
+  @GetMapping("/tracker")
+  public String trackerForm(final Model model) {
+    model.addAttribute("abweichung", new Abweichung());
+    return "tracker";
+  }
+
+  @PostMapping("/tracker")
+  public String trackerSubmit(@ModelAttribute final Abweichung abweichung) {
+    log.debug("{}", abweichung);
+    return "overview";
+  }
+
+}
