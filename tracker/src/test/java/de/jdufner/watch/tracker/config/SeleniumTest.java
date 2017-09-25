74723990271bf1889fbb0194a02578b5b7a8351d
Juergen Dufner <juergen@jdufner.de>
2017-09-25 20:57:54 +0200

    prepare for Selenium tests and reload Thymeleaf resources dynamically

---
+++ tracker/src/test/java/de/jdufner/watch/tracker/config/SeleniumTest.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2017.
+ *
+ * This file is part of Watchtracker.
+ *
+ * Watchtracker is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Watchtracker is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Watchtracker.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Diese Datei ist Teil von Watchtracker.
+ *
+ * Watchtracker ist Freie Software: Sie können es unter den Bedingungen
+ * der GNU General Public License, wie von der Free Software Foundation,
+ * Version 3 der Lizenz oder (nach Ihrer Wahl) jeder späteren
+ * veröffentlichten Version, weiterverbreiten und/oder modifizieren.
+ *
+ * Watchtracker wird in der Hoffnung, dass es nützlich sein wird, aber
+ * OHNE JEDE GEWÄHRLEISTUNG, bereitgestellt; sogar ohne die implizite
+ * Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
+ * Siehe die GNU General Public License für weitere Details.
+ *
+ * Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
+ * Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
+ */
+
+package de.jdufner.watch.tracker.config;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author Jürgen Dufner
+ * @since 0.3
+ */
+@Documented
+@Inherited
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+//@TestExecutionListeners(
+//    listeners = SeleniumTestExecutionListener.class,
+//    mergeMode = MERGE_WITH_DEFAULTS)
+public @interface SeleniumTest {
+
+  Class<? extends WebDriver> driver() default FirefoxDriver.class;
+
+  String baseUrl() default "http://localhost:8080";
+
+}
